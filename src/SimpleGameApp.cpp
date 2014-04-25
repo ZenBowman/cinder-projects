54e2f08aeada2d5aa620ebee50c29746a65609c8
prasan.samtani <prasan.samtani@hulu.com>
2014-04-25 13:02:03 -0700

    Initial import of SimpleApp

--- src/SimpleGameApp.cpp
+++ src/SimpleGameApp.cpp
@@ -1,20 +1,35 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class SimpleGameApp : public AppNative {
-  public:
+public:
+  void prepareSettings(Settings *settings);
 	void setup();
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+  
+private:
+  gl::Texture backgroundImage;
 };
+
+void SimpleGameApp::prepareSettings(Settings *settings)
+{
+  settings->setWindowSize(WIDTH, HEIGHT);
+}
 
 void SimpleGameApp::setup()
 {
+  Url url("http://www.durationator.com/img/background-green.jpg");
+  backgroundImage = gl::Texture(loadImage(loadUrl(url)));
 }
 
 void SimpleGameApp::mouseDown( MouseEvent event )
@@ -28,7 +43,11 @@ void SimpleGameApp::update()
 void SimpleGameApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+  
+  backgroundImage.enableAndBind();
+  gl::draw(backgroundImage, getWindowBounds());
+  
 }
 
 CINDER_APP_NATIVE( SimpleGameApp, RendererGl )
